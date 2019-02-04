f1aaefa5aff877732deacab1f6bc7b2cc5b7d77a
KyleSieb <KyleSieb@users.noreply.github.com>
2019-02-04 12:09:04 -0500

    Date Formatter

---
+++ ArgoOut/New Group/ScheduleHandler.swift
@@ -0,0 +1,38 @@
+//
+//  ScheduleHandler.swift
+//  RPSSignOut
+//
+//  Created by Matthew Romage on 2/4/19.
+//  Copyright © 2019 Lake. All rights reserved.
+//
+
+import Foundation
+
+class ScheduleHandler
+{
+    // MARK: - Declarations
+    let date = Date()
+    let dateFormatter = DateFormatter()
+    
+    // MARK: - Constructor
+    init()
+    {
+        // Specify wanted date format
+        dateFormatter.dateStyle = .short
+        dateFormatter.timeStyle = .none
+        dateFormatter.locale = Locale(identifier: "en_US")
+    }
+    
+    // MARK: - Methods
+    func findTodaysDate() -> String
+    {
+        // Format today's date
+        let formattedDate = dateFormatter.string(from: date)
+        
+        // Replace "/" with "_"
+        return formattedDate.replacingOccurrences(of: "/", with: "_")
+    }
+    
+    
+    
+}
